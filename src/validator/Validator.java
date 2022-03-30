24e5fdf88d025241e12a7d1604277e64db503241
Lilit <amiryanlilart@gmail.com>
2022-03-30 22:20:56 +0400

    Validator change

--- src/validator/Validator.java
+++ src/validator/Validator.java
@@ -1,33 +1,38 @@
 package validator;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validator {
-    public static final Pattern NAME_PATTERN = Pattern.compile("(.*){1,50}$");
-    public static final Pattern PHONE_NUMBER_PATTERN = Pattern.compile(
+    public static final Pattern VALID_NAME_PATTERN = Pattern.compile("(.*){1,50}$");
+    public static final Pattern VALID_PHONE_NUMBER_PATTERN = Pattern.compile(
             "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$"
-            + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"
-            + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$");
-    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+                    + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"
+                    + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$");
+    public static final Pattern VALID_EMAIL_PATTERN =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
-    public static boolean isValidChoice(String choice) {
-        return true;
+    public static boolean isValidRange(int min, int max, int choice) {
+            return (choice>=min && choice<=max);
     }
 
     public static boolean isValidContactName(String contactName) {
-        return true;
+        Matcher matcher = VALID_NAME_PATTERN.matcher(contactName);
+        return matcher.matches();
     }
 
     public static boolean isValidPhoneNumber(String phoneNumber) {
-        return true;
+        Matcher matcher = VALID_PHONE_NUMBER_PATTERN.matcher(phoneNumber);
+        return matcher.matches();
     }
 
     public static boolean isValidEmail(String email) {
-        return true;
+        Matcher matcher = VALID_EMAIL_PATTERN.matcher(email);
+        return matcher.matches();
     }
 
     public static boolean isValidCompanyName(String companyName) {
-        return true;
+        Matcher matcher = VALID_NAME_PATTERN.matcher(companyName);
+        return matcher.matches();
     }
 }
